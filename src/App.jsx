49b93ac1773af1c565f980eb40a2cf7f8a736edf
Khambazarov <khambazarovrenat@gmail.com>
2024-04-24 21:36:11 +0200

    add robohash's & create new design

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,149 @@
 import { useState } from "react";
 import "./App.css";
+import Ali from "./assets/robohash/Ali.png";
+import Alper from "./assets/robohash/Alper.png";
+import Liang from "./assets/robohash/Liang.png";
+import Matthias from "./assets/robohash/Matthias.png";
+import Mohammad from "./assets/robohash/Mohammad.png";
+import Murat from "./assets/robohash/Murat.png";
+import Nasrin from "./assets/robohash/Nasrin.png";
+import Olha from "./assets/robohash/Olha.png";
+import Olivia from "./assets/robohash/Olivia.png";
+import Renat from "./assets/robohash/Renat.png";
+import Sebastian from "./assets/robohash/Sebastian.png";
+import Sergen from "./assets/robohash/Sergen.png";
+
+const robohash = {
+  Ali,
+  Alper,
+  Liang,
+  Matthias,
+  Mohammad,
+  Murat,
+  Nasrin,
+  Olha,
+  Olivia,
+  Renat,
+  Sebastian,
+  Sergen,
+};
 
 function App() {
-  const [participants, setParticipants] = useState("");
+  const [students, setStudents] = useState([]);
+  const [selectedStudents, setSelectedStudents] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const studentsArray = [
+    "Ali",
+    "Alper",
+    "Liang",
+    "Matthias",
+    "Mohammad",
+    "Murat",
+    "Nasrin",
+    "Olha",
+    "Olivia",
+    "Renat",
+    "Sebastian",
+    "Sergen",
+  ];
+
+  const handleCheckboxChange = (event) => {
+    const { name, checked } = event.target;
+    setSelectedStudents((prev) =>
+      checked ? [...prev, name] : prev.filter((student) => student !== name)
+    );
+  };
+
+  const handleSelectAll = (event) => {
+    const { checked } = event.target;
+    setSelectedStudents(checked ? studentsArray : []);
+  };
+
+  const randomizeArray = (array) => {
+    const randomizedArray = [...array];
+    for (let i = randomizedArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [randomizedArray[i], randomizedArray[j]] = [
+        randomizedArray[j],
+        randomizedArray[i],
+      ];
+    }
+    return randomizedArray;
+  };
 
   const randomize = () => {
-    const participants = {
-      1: "Ali",
-      2: "Alper",
-      3: "Liang",
-      4: "Matthias",
-      5: "Mohammad",
-      6: "Murat",
-      7: "Nasrin",
-      8: "Olha",
-      9: "Olivia",
-      10: "Renat",
-      11: "Sebastian",
-      12: "Sergen",
-    };
-
-    const amountOfParticipants = Object.keys(participants).length;
-
-    const randomParticipant = Math.ceil(Math.random() * amountOfParticipants);
-
-    return setTimeout(() => {
-      setParticipants(participants[randomParticipant]);
-    }, 500);
+    setLoading(true);
+    setTimeout(() => {
+      const randomizedStudents = randomizeArray(selectedStudents);
+      setStudents(randomizedStudents);
+      setLoading(false);
+    }, 3000);
   };
-  clearTimeout();
 
   const reset = () => {
-    setParticipants("");
+    setStudents([]);
+    setSelectedStudents([]);
   };
 
   return (
     <>
-      <h1>Random Generator</h1>
-      <div className="wrapper">
-        <button onClick={randomize} className="btn">
-          Choose Random Student
-        </button>
-        <button onClick={reset} className="btn">
-          Reset
-        </button>
-        {!participants ? <h2>?</h2> : <h2>{participants}</h2>}
-      </div>
+      {loading ? (
+        <div className="loader">
+          <h1 className="loading">Wer darf antreten?</h1>
+        </div>
+      ) : students.length === 0 ? (
+        <div className="wrapper">
+          <button onClick={randomize} className="btn">
+            Randomize Students
+          </button>
+          <h1 className="heading">Select Students </h1>
+          <label>
+            <input
+              type="checkbox"
+              name="selectAll"
+              onChange={handleSelectAll}
+            />
+            Select All
+          </label>
+          {studentsArray.map((student) => (
+            <label key={student}>
+              <input
+                type="checkbox"
+                name={student}
+                checked={selectedStudents.includes(student)}
+                onChange={handleCheckboxChange}
+              />
+              {student}
+            </label>
+          ))}
+        </div>
+      ) : (
+        <div className="wrapper">
+          <button onClick={reset} className="btn">
+            Reset Students
+          </button>
+          <ul>
+            {students.map((student, index) => (
+              <>
+                <li key={index}>
+                  <img
+                    className="robohash"
+                    src={robohash[student]}
+                    alt="robohash"
+                  />
+                  {student}
+                  <img
+                    className="robohash"
+                    src={robohash[student]}
+                    alt="robohash"
+                  />
+                </li>
+              </>
+            ))}
+          </ul>
+        </div>
+      )}
     </>
   );
 }
